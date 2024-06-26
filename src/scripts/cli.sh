910a6b0155d982589fd769b4cc43b162a126276b
Garret Patten <garret.patten@proton.me>
2024-06-25 21:53:11 -0400

    Add jq install

--- src/scripts/cli.sh
+++ src/scripts/cli.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cliTools=("bat" "curl" "exa" "exiftool" "eza" "fastfetch" "fd" "git" "htop" "neovim" "openvpn" "ripgrep" "tmux" "vim" "wget" "zsh")
+cliTools=("bat" "curl" "exa" "exiftool" "eza" "fastfetch" "fd" "git" "htop" "jq" "neovim" "openvpn" "ripgrep" "tmux" "vim" "wget" "zsh")
 for tool in ${cliTools[@]}; do
     if [[ ! -d "/usr/local/cellar/$tool/" ]]; then
         brew install "$tool"
