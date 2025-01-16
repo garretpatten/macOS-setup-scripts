e8cd56c9d0da0215184358d75fbf7eeb3f8d9e5f
Garret Patten <garret.patten@ncino.com>
2025-01-16 12:12:33 -0500

    chore: remove spectacle -- it's no longer maintained and its funcionality is replaced with Raycast window extensions

--- src/scripts/productivity.sh
+++ src/scripts/productivity.sh
@@ -10,40 +10,6 @@ if [[ ! -d "usr/local/cellar/raycast/" ]]; then
     brew install raycast
 fi
 
-# Spectacle
-if [[ ! -d "usr/local/cellar/spectacle/" ]]; then
-    brew install spectacle
-fi
-
-# Taskwarrior
-if [[ ! -d "usr/local/cellar/task/" ]]; then
-    brew install task
-
-    # Handle first prompt (to create config file)
-    echo "yes" | task
-
-    # Update ~/.taskrc
-    cat "$workingDirectory/src/dotfiles/taskwarrior/.taskrc-additions" >> "$HOME/.taskrc"
-
-    # Add manual setup tasks
-    task add Log in to iCloud project:mac priority:H
-    task add Export GitHub PAT with 1Password project:dev priority:H
-    task add Clean up .zshrc file project:setup priority:H
-    task add Disable Spotlight hotkey in favor or Raycast project:setup priority:H
-
-    task add Install Notion project:PWAs priority:M
-    task add Configure 1Password project:setup priority:M
-
-    task add Download needed files from Proton Drive project:setup priority:L
-    task add Configure VS Code settings project:dev priority:L
-fi
-
-# Create directory for custom taskwarrior themes
-if [[ ! -d "$HOME/.task/themes/" ]]; then
-    mkdir "$HOME/.task/themes/"
-    cp "$(pwd)/src/dotfiles/taskwarrior/themes/" "$HOME/.task/themes/"
-fi
-
 # Todoist
 if [[ -d "/opt/homebrew/Caskroom/todoist/" ]]; then
     brew install --cask todoist
