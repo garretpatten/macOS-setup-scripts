16b5f97f0e75b293d26c120e8d57fad3da0e8f4e
Garret Patten <garret.patten@proton.me>
2025-03-27 17:29:15 -0400

    feat: add Chat GPT install

--- src/scripts/misc.sh
+++ src/scripts/misc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Balena Etcher, Zoom
-caskApps=("balenaetcher" "zoom")
+caskApps=("balenaetcher" "chatgpt" "zoom")
 for caskApp in ${caskApps[@]}; do
     if [[ ! -d "usr/local/Caskroom/$caskApp/" ]]; then
         brew install --cask "$caskApp"
