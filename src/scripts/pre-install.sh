56c69fd54131e92e21004f49db522fc58e4f4def
Garret Patten <garret.patten@proton.me>
2025-03-27 21:43:58 -0400

    chore: cosmetic

--- src/scripts/pre-install.sh
+++ src/scripts/pre-install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# Xcode Command Line Tools Fresh Install
+# Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew analytics off
+
+# System updates
+brew update && brew upgrade && brew cleanup
+
+# X Code
 xcodePath="/Library/Developer/CommandLineTools/"
 
 if [[ -d "$xcodePath" ]]; then
@@ -10,10 +17,3 @@ fi
 sudo xcode-select --install
 sudo xcodebuild -license accept
 softwareupdate --all --install --force
-
-# Homebrew package manager
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew analytics off
-
-# System updates
-brew update && brew upgrade && brew cleanup
